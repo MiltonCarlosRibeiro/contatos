09f7cfbb8d21e75aef2cf01eb632ff2d15fee1c8
MiltonCarlosRibeiro <milton.carlos.dev@gmail.com>
2025-04-21 18:10:14 -0300

    Consulta de dados com JPQL

--- src/main/java/br/com/fiap/contatos/ContatoApp.java
+++ src/main/java/br/com/fiap/contatos/ContatoApp.java
@@ -4,24 +4,31 @@ import br.com.fiap.contatos.model.Contato;
 import br.com.fiap.dao.Conexao;
 import br.com.fiap.dao.ContatoDao;
 import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.Persistence;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public class ContatoApp {
     public static void main(String[] args) {
-        Contato contato = new Contato();
-        contato.setNome("Fabiana Oliveira");
-        contato.setEmail("fabiana@email.com.br");
-        contato.setDataNascimento(LocalDate.of(2000,10,15));
 
-
-        // Criação do Entity Manager
-
+         /*Criação do Entity Manager*/
         EntityManager em = Conexao.getEntityManager();
+        //cadastrar (em);
+        //atualizar(em);
+        //excluir(em);
+        //consutarContatoPorId(em);
+        //listarTodosOsContatos(em);
+        listaContatoPorEmail(em);
 
-        //Criar uma Instancia do Dao
+    }
+    /*CADASTRAR*/
+    public static void cadastrar (EntityManager em){
+        Contato contato = new Contato();
+        contato.setNome("Hugo Correa Carlos");
+        contato.setEmail("hugo@fiap.com.br");
+        contato.setDataNascimento(LocalDate.of(1999,10,15));
+
+        /*Criar uma Instancia do Dao*/
         ContatoDao contatoDao = new ContatoDao(em);
 
         em.getTransaction().begin();
@@ -29,5 +36,81 @@ public class ContatoApp {
         em.getTransaction().commit();
 
     }
+    /*ATUALIZAR*/
+    public static void atualizar (EntityManager em){
+        Contato contato = new Contato();
+        contato.setId(2L);
+        contato.setNome("Fabiana Rodrigues");
+        contato.setEmail("victor@fiap.com.br");
+        contato.setDataNascimento(LocalDate.of(2000,10,15));
+
+        /*Criar uma Instancia do Dao*/
+        ContatoDao contatoDao = new ContatoDao(em);
+
+        em.getTransaction().begin();
+        contatoDao.atualizar(contato);
+        em.getTransaction().commit();
+
+    }
+    /*EXCLUIR*/
+    public static void excluir (EntityManager em){
+        Contato contato = new Contato();
+        contato.setId(1L);;
+
+        /*Criar uma Instancia do Dao*/
+        ContatoDao contatoDao = new ContatoDao(em);
+
+        em.getTransaction().begin();
+        contatoDao.excluir(contato);
+        em.getTransaction().commit();
+
+    }
+
+    /*CONSULTAS*/
+    public static void consutarContatoPorId (EntityManager em){
+
+
+        /*Criar uma Instancia do Dao*/
+        ContatoDao contatoDao = new ContatoDao(em);
+
+        em.getTransaction().begin();
+        contatoDao.consultaContatoPorId(3L);
+        em.getTransaction().commit();
+
+    }
+
+    public static void listarTodosOsContatos (EntityManager em){
+
+
+        /*Criar uma Instancia do Dao*/
+        ContatoDao contatoDao = new ContatoDao(em);
+
+        List<Contato> contatos = contatoDao.listarTodosOsContatos();
+        for (Contato contato : contatos) {
+            System.out.println("********************************************************************************************************");
+            System.out.println(contato.toString());
+        }
+        System.out.println("********************************************************************************************************");
+        System.out.println("Fim dos registros...");
+
+    }
+
+    public static void listaContatoPorEmail (EntityManager em){
+
+
+        /*Criar uma Instancia do Dao*/
+        ContatoDao contatoDao = new ContatoDao(em);
+
+        List<Contato> contatos = contatoDao.listarContatosPorEmail("victor@fiap.com.br");
+        for (Contato contato : contatos) {
+            System.out.println("********************************************************************************************************");
+            System.out.println(contato.toString());
+        }
+        System.out.println("********************************************************************************************************");
+        System.out.println("Fim dos registros...");
+
+    }
+
+
 
 }
--- src/main/java/br/com/fiap/dao/ContatoDao.java
+++ src/main/java/br/com/fiap/dao/ContatoDao.java
@@ -3,6 +3,8 @@ package br.com.fiap.dao;
 import br.com.fiap.contatos.model.Contato;
 import jakarta.persistence.EntityManager;
 
+import java.util.List;
+
 public class ContatoDao {
 
     private EntityManager em;
@@ -14,6 +16,50 @@ public class ContatoDao {
     /*SALVAR*/
     public void salvar(Contato contato) {
         em.persist(contato);
+    }
+
+    /*ATUALIZAR*/
+    public void atualizar(Contato contato) {
+        em.merge(contato);
+    }
+
+    /*EXCLUIR*/
+    public void excluir(Contato contato) {
+        Contato contatoExcluir = em.find(Contato.class, contato.getId());
+        em.remove(contatoExcluir);
+    }
+
+    /*CONSULTAS*/
+    public void consultaContatoPorId(Long id){
+        Contato contatoConsulta = em.find(Contato.class, id);
+
+        if(contatoConsulta == null){
+            System.out.println("contato nao encontrado!!");
+        }else {
+            System.out.println("*********************************************************************************************************");
+            System.out.println(contatoConsulta.toString());
+            System.out.println("*********************************************************************************************************");
+        }
+
+    }
+
+    public List<Contato> listarTodosOsContatos(){
+        /*SQL ----> SELECT * FROM tbl_contatos ORDER BY nome ASC*/
+
+        /*JPQL*/
+        String consulta = "SELECT c FROM Contato c ORDER BY nome ASC";
+        return em.createQuery(consulta).getResultList();
+    }
+
+    public List<Contato> listarContatosPorEmail(String emailProcurado){
+        /*SQL ----> SELECT email FROM tbl_contatos ORDER BY nome ASC*/
+
+        /*JPQL*/
+        String consulta = "SELECT c FROM Contato c WHERE email = :email";
+
+        return em.createQuery(consulta, Contato.class)
+                .setParameter("email", emailProcurado)
+                .getResultList();
     }
 
 
